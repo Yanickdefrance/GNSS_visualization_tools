e6ea4c564c44c1a908948071556ad75694b4bfb8
Yanickdefrance <yannickdef.yd@gmail.com>
2016-07-26 16:04:08 +0300

    Update index.py

--- index.py
+++ index.py
@@ -31,11 +31,6 @@ html = """<!DOCTYPE html>
     <article id="run">
         <h2>Run current scenario</h2>
         <a href="http://localhost:63342/Web%20page/map_1.html"><p>Visualize the chosen scenario</p></a>
-
-"""
-print(html)
-print('Current scenario : '+str(form.getvalue("select")))
-html2="""
     </article>
     <article id="create">
         <h2>Create scenario</h2>
@@ -50,6 +45,7 @@ html2="""
                     <option value="Circle">Circle</option>
                     <option value="Square">Square</option>
                     <option value="Line">Line</option>
+                    <option value="Static">Static</option>
                 </optgroup>
             </select>
             <input type="submit" name="change" value="Change scenario">
@@ -57,58 +53,76 @@ html2="""
     </article>
 </section>
 
-
+<fieldset>
+    <legend>Visualisation of the
 """
 
-print(html2)
-
-html3 ="""
-
-<fieldset>
-    <legend>Title</legend>
+html2="""
+    scenario</legend>
     <div id="mapid"></div>
-    <script src="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"></script>
+    <script src="sample-geojson.js" type="text/javascript"></script>
+	<script src="https://npmcdn.com/leaflet@1.0.0-rc.2/dist/leaflet.js"></script>
 	<script>
 
 		var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-        L.marker([51.5, -0.09]).addTo(mymap)
-			.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-
 		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-			maxZoom: 18,
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 				'Imagery <a href="http://mapbox.com">Mapbox</a>',
 			id: 'mapbox.streets'
 		}).addTo(mymap);
+"""
 
-    	L.marker([51.5, -0.09]).addTo(mymap)
-			.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-
-		L.circle([51.508, -0.11], 500, {
-			color: 'red',
-			fillColor: '#f03',
-			fillOpacity: 0.5
-		}).addTo(mymap).bindPopup("I am a circle.");
-
-		L.polygon([
-			[51.509, -0.08],
-			[51.503, -0.06],
-			[51.51, -0.047]
-		]).addTo(mymap).bindPopup("I am a polygon.");
+print(html+str(form.getvalue("select"))+html2)
 
 
-		var popup = L.popup();
+if form.getvalue("select") == 'None' :
+    P = project2.position.data('project2/ublox/ublox_data_Static.txt')
+    Q = project2.position.data('project2/spectracom/spectracom_data_Static.txt')
+else :
+    P = project2.position.data('project2/ublox/ublox_data_'+str(form.getvalue("select"))+'.txt')
+    Q = project2.position.data('project2/spectracom/spectracom_data_'+str(form.getvalue("select"))+'.txt')
 
-		function onMapClick(e) {
-			popup
-				.setLatLng(e.latlng)
-				.setContent("You clicked the map at " + e.latlng.toString())
-				.openOn(mymap);
-		}
 
-        mymap.on('click', onMapClick);
+a = []
+b = []
+for i in range(len(P)):
+    a.append([P[i][2],P[i][1]])
+
+for i in range(len(Q)):
+    b.append([Q[i][2],Q[i][1]])
+
+
+html4="""
+
+        var myLines = [{
+            "type": "LineString",
+            "color":"#FF0000",
+            "coordinates":"""
+
+html5="""
+        }, {
+            "type": "LineString",
+            "coordinates":"""
+
+html6="""
+        }];
+
+        var myStyle = {
+            "color": "#ff7800"
+        };
+
+        var multipolygon = L.geoJson(myLines);
+        multipolygon.addTo(mymap);
+        mymap.fitBounds(multipolygon.getBounds());
+
+        $(window).on("resize", function() {
+            $("#map").height($(window).height()).width($(window).width());
+            map.invalidateSize();
+        }).trigger("resize");
+
+        mymap.on('click', newMarker);
 
 	</script>
 </fieldset>
@@ -118,10 +132,7 @@ html3 ="""
 </html>
 """
 
-print(html3)
-
-P = project2.position.data('project2/ublox_data_'+str(form.getvalue("select"))+'.txt')
-Q = project2.position.data2('project2/ublox_data_'+str(form.getvalue("select"))+'.txt')
-
+print(html4+str(a)+html5+str(b)+html6)
 
 project2.create_map.create_map(P,Q)
+
